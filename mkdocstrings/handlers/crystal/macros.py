c984212d23bfa4bbf6e5e70a1dff90d023c9af23
Oleh Prypin <oleh@pryp.in>
2021-01-09 00:24:02 +0100

    Fix code formatting

--- mkdocstrings/handlers/crystal/macros.py
+++ mkdocstrings/handlers/crystal/macros.py
@@ -1,4 +1,6 @@
 def define_env(env):
     """Pluglet for mkdocs-macros plugin."""
 
-    env.variables['crystal'] = env.conf['plugins']['mkdocstrings'].get_handler('crystal').collector.root
+    env.variables["crystal"] = (
+        env.conf["plugins"]["mkdocstrings"].get_handler("crystal").collector.root
+    )
