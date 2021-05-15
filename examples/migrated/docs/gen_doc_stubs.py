b76d0c9a4a33e1811e21fac4df52f0d4f0f59891
Oleh Prypin <oleh@pryp.in>
2021-05-15 19:20:25 +0200

    Show the new nav generator from mkdocs-gen-files

--- examples/migrated/docs/gen_doc_stubs.py
+++ examples/migrated/docs/gen_doc_stubs.py
@@ -4,15 +4,15 @@
 import mkdocs_gen_files
 
 # Get the documentation root object
-root = mkdocs_gen_files.config['plugins']['mkdocstrings'].get_handler('crystal').collector.root
+root = mkdocs_gen_files.config["plugins"]["mkdocstrings"].get_handler("crystal").collector.root
 
 # For each type (e.g. "Foo::Bar")
 for typ in root.walk_types():
     # Use the file name "Foo/Bar/index.md"
-    filename = '/'.join(typ.abs_id.split('::') + ['index.md'])
+    filename = "/".join(typ.abs_id.split("::") + ["index.md"])
     # Make a file with the content "# ::: Foo::Bar\n"
-    with mkdocs_gen_files.open(filename, 'w') as f:
-        print(f'# ::: {typ.abs_id}', file=f)
+    with mkdocs_gen_files.open(filename, "w") as f:
+        print(f"# ::: {typ.abs_id}", file=f)
 
     # Link to the type itself when clicking the "edit" button on the page.
     if typ.locations:
--- examples/migrated/docs/gen_doc_stubs_nav.py
+++ examples/migrated/docs/gen_doc_stubs_nav.py
@@ -4,23 +4,26 @@
 import mkdocs_gen_files
 
 # Get the documentation root object
-root = mkdocs_gen_files.config['plugins']['mkdocstrings'].get_handler('crystal').collector.root
+root = mkdocs_gen_files.config["plugins"]["mkdocstrings"].get_handler("crystal").collector.root
 
-# Start a navigation file (to be filled as we go along)
-nav = mkdocs_gen_files.open('SUMMARY.md', 'a')
+# Start a navigation collection (to be filled as we go along)
+nav = mkdocs_gen_files.Nav()
 
 # For each type (e.g. "Foo::Bar")
 for typ in root.walk_types():
     # Use the file name "Foo/Bar.md"
-    filename = '/'.join(typ.abs_id.split('::')) + '.md'
+    filename = "/".join(typ.abs_id.split("::")) + ".md"
     # Make a file with the content "# ::: Foo::Bar\n"
-    with mkdocs_gen_files.open(filename, 'w') as f:
-        print(f'# ::: {typ.abs_id}', file=f)
+    with mkdocs_gen_files.open(filename, "w") as f:
+        print(f"# ::: {typ.abs_id}", file=f)
 
     # Link to the type itself when clicking the "edit" button on the page
     if typ.locations:
         mkdocs_gen_files.set_edit_path(filename, typ.locations[0].url)
 
     # Append to the nav: "    * [Bar](Foo/Bar.md)"
-    indent = '    ' * typ.abs_id.count('::')
-    print(indent + f'* [{typ.name}]({filename})', file=nav)
+    nav[typ.abs_id.split("::")] = filename
+
+# Append the nav to a "literate nav" file
+with mkdocs_gen_files.open("SUMMARY.md", "a") as nav_file:
+    nav_file.writelines(nav.build_literate_nav())
